5c9f9fdb8c72fa673ea6ef1ce340afe414fc8af9
Viktor Petevotov <bloadvenro@mail.ru>
2018-09-12 15:09:06 +0300

    categorized exports

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
-export * from './lib/aliases';
-export * from './lib/dictionaries';
-export * from './lib/functions';
-export * from './lib/meta';
-export * from './lib/sets';
-export * from './lib/tuples';
+import * as Aliases from './lib/aliases';
+import * as Dictionaries from './lib/dictionaries';
+import * as Functions from './lib/functions';
+import * as Meta from './lib/meta';
+import * as Sets from './lib/sets';
+import * as Tuples from './lib/tuples';
+
+export {
+  Aliases,
+  Dictionaries,
+  Functions,
+  Meta,
+  Sets,
+  Tuples
+}
